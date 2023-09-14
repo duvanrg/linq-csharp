04ddd29dde68cfd30e78757329e252b1ffe95166
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-09-14 13:11:58 -0500

    feat: :sparkles: class 14 sept
    class linqQueries
    
    @duvanrg

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,7 +1,56 @@
-internal class LinqQueries
+
+public class LinqQueries
 {
-    private static void Main(string[] args)
+#nullable disable
+    List<Book> lstBooks = new List<Book>();
+    public LinqQueries()
     {
-        int a = 0;
+        using (StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.lstBooks = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true }) ?? new List<Book>();
+        }
     }
+
+    public IEnumerable<Book> AllCollection()
+    {
+        return lstBooks;
+    }
+
+    public IEnumerable<Book> LibrosDespues2000()
+    {
+        return from book in lstBooks where book.PublishedDate.Year > 2000 select book;
+    }
+    public IEnumerable<Book> OnlyAndroid()
+    {
+        return from book in lstBooks where book.Title.Contains("Android") select book;
+    }
+    public IEnumerable<Book> OnlyAndroid2005()
+    {
+        return from book in lstBooks where book.Title.Contains("Android") && book.PublishedDate.Year > 2005 select book;
+    }
+    public IEnumerable<Book> page250Action()
+    {
+        return from book in lstBooks where book.Title.Contains("Action") && book.PageCount > 250 select book;
+    }
+    public bool TrueStatus()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        return lstBooks.All(book => book.Status != null);
+    }
+    public IEnumerable<Book> publish2005()
+    {
+        if (Truepublish2005())
+            return from book in lstBooks where book.PublishedDate.Year == 2005 select book;
+        Console.WriteLine("No Existen libros publicados en el 2005");
+        Console.ReadKey();
+        return new List<Book>();
+
+    }
+    public bool Truepublish2005()
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        return lstBooks.Any(book => book.PublishedDate.Year == 2005);
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-System.LinqQueries.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int?[] args2 = new int?[5];
+
+        Console.WriteLine(args2[0]);
+        LinqQueries queries = new LinqQueries();
+        
+        ImprimirValores(queries.publish2005());
+        // Console.WriteLine(queries.TrueStatus() ? "Todos los libros tienen Status" : "Almenos uno de los libros no tiene Status");
+    }
+
+    private static void ImprimirValores(IEnumerable<Book> books)
+    {
+        int registros = 0;
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("{0, -70} {1,7} {2,20}", "Titulo", "N. Paginas", "Fecha Publicacion");
+        foreach (Book book in books)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            registros += 1;
+            Console.WriteLine("{0, -70} {1,7} {2,20}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+        }
+    }
+    enum Color {
+        Rojo,
+        Verde
+    }
+
+    private static void ImprimirGrupos(IEnumerable<IGrouping<int, Book>> books)
+    {
+        foreach (var grp in books)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Grupo Nro : {grp.Key}");
+            Console.WriteLine("{0, -70} {1,7} {2,20}", "Titulo", "N. Paginas", "Fecha Publicacion");
+            foreach (var grpo in grp)
+            {
+            }
+        }
+    }
+}
